3ccc44f5fba8e50e423e252f7b7fea572f11fff9
V Jeswanth Murthy <jeswanthmurthy0309@gmail.com>
2021-02-15 11:55:39 +0530

    updated

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ import csv
 
 f = open('Level_1_data\input.json', )
 data = json.load(f)
+ans = []
 for i in range(1,data['die']['columns']+1):
     im = Image.open('Level_1_data\wafer_image_'+str(i)+'.png')
     px = im.load()
     width,height = im.size
     numpydata = numpy.array(im)
-    ans = []
+
     r = [data['care_areas'][0]['top_left']['x'],data['care_areas'][0]['bottom_right']['x']]
     for x in range(len(numpydata)):
         d = cc.defaultdict(int)
-        for y in range(len(numpydata[i])):
-            d[numpydata[x][y][0]] += 1
+        for y in range(len(numpydata[x])):
+            d[str(numpydata[x][y])] += 1
         m = 0
         for z in d:
             if(d[z] > m):
-                temp = x
+                temp = z
                 m = d[z]
         if(x >= r[0] and x <= r[1]):
-            for y in range(data['care_areas'][0]['bottom_right']['y'],data['care_areas'][0]['top_left']['y']):
-                if(numpydata[x][y][0] != temp):
+            for y in range(data['care_areas'][0]['bottom_right']['y'],data['care_areas'][0]['top_left']['y']+1):
+                if(str(numpydata[x][y]) != str(temp)):
                     ans.append([i,x,y])
     print(ans)
 with open("final.csv",'w') as csvfile:

a2a5a0a900507a6b758714d70ee5d0450a5401c0
V Jeswanth Murthy <jeswanthmurthy0309@gmail.com>
2021-02-15 11:41:44 +0530

    updated

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import json
 from PIL import Image
 import numpy
 import collections as cc
+import csv
 
-f = open('D:\kla\Level_1_data\input.json', )
+f = open('Level_1_data\input.json', )
 data = json.load(f)
-
 for i in range(1,data['die']['columns']+1):
-    im = Image.open('D:\kla\Level_1_data\wafer_image_'+str(i)+'.png')
+    im = Image.open('Level_1_data\wafer_image_'+str(i)+'.png')
     px = im.load()
     width,height = im.size
     numpydata = numpy.array(im)
     ans = []
-    r = list(range(data['care_areas'][0]['top_left']['x'],data['care_areas'][0]['bottom_right']['x']))
-    print(r)
+    r = [data['care_areas'][0]['top_left']['x'],data['care_areas'][0]['bottom_right']['x']]
     for x in range(len(numpydata)):
         d = cc.defaultdict(int)
         for y in range(len(numpydata[i])):
@@ -23,10 +22,13 @@ for i in range(1,data['die']['columns']+1):
             if(d[z] > m):
                 temp = x
                 m = d[z]
-        for y in range(len(numpydata[i])):
-            if(numpydata[i][y][0] != temp):
-                ans.append([i,x,y])
-
-
+        if(x >= r[0] and x <= r[1]):
+            for y in range(data['care_areas'][0]['bottom_right']['y'],data['care_areas'][0]['top_left']['y']):
+                if(numpydata[x][y][0] != temp):
+                    ans.append([i,x,y])
+    print(ans)
+with open("final.csv",'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(ans)
 
 f.close()
